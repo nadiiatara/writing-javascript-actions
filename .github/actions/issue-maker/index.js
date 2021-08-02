4fdfe4acc620782a2fa7c5996930fa394b7b22d2
nadiiatara <64203855+nadiiatara@users.noreply.github.com>
2021-08-02 18:20:11 +0300

    additional update index.js

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -7,13 +7,13 @@ async function run() {
         const jokeBody = core.getInput("joke");
         const token = core.getInput("repo-token");
 
-        const octokit = new github.getOctokit(token);
+        const octokit = github.getOctokit(token);
 
         const newIssue = await octokit.rest.issues.create({
             repo: github.context.repo.repo,
             owner: github.context.repo.owner,
             title: issueTitle,
-            body: jokeBody
+            body; jokeBody
         });
     } catch (error) {
         core.setFailed(error.message);
