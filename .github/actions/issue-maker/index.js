511ed086eea46b402ffae8d43e26c0a0a73c8474
nadiiatara <64203855+nadiiatara@users.noreply.github.com>
2021-08-02 16:53:55 +0300

    update again  index.js

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -2,22 +2,19 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 async function run() {
-    console.log(`Joke input:` + core.getInput("joke"));
     try {
         const issueTitle = core.getInput("issue-title");
         const jokeBody = core.getInput("joke");
         const token = core.getInput("repo-token");
 
-        console.log(jokeBody);
-
         const octokit = new github.getOctokit(token);
-        const newIssue  = await octokit.request('POST /repos/{owner}/{repo}/issues', {
+
+        const newIssue = await octokit.issues.create({
             repo: github.context.repo.repo,
             owner: github.context.repo.owner,
             title: issueTitle,
             body: jokeBody
         });
-        console.log(newIssue)
     } catch (error) {
         core.setFailed(error.message);
     }
